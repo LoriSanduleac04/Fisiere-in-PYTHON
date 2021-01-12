513fdaf051c9148dc0053f36c98d86429b0ed765
Loredana Sanduleac <70658391+LoriSanduleac04@users.noreply.github.com>
2021-01-12 19:14:02 +0200

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,8 +1,7 @@
 """ 
-Un brăduţ este împodobit cu globuleţe albe, roşii şi albastre. Numărul globuleţelor
-albe se citeşte de la tastatură. Câte globuleţe are brăduţul, ştiind că numărul de
-globuleţe roşii este cu 3 mai mare decât numărul de globuleţe albe, iar globuleţele
-albastre sunt cu 2 mai puţine decât totalul celor albe şi roşii.
+Un brăduţ este împodobit cu globuleţe albe, roşii şi albastre. Numărul globuleţelor albe se citeşte din fişierul « globulet.txt ». Câte globuleţe are brăduţul, ştiind că numărul de globuleţe roşii este cu 3 mai mare decât numărul de globuleţe albe, iar globuleţele albastre sunt cu 2 mai puţine decât totalul celor albe şi roşii. Numărul total de globuleţe este înscris în fişierul « bradut.txt »
+Exemplu: Date de intrare: 12 	Date de ieşire: 52.
+
 """
 with open ('globulete.txt','r') as f:
     a=f.readline()
